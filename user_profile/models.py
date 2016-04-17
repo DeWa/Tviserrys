175ffb66a58d8f05150a50b2a6dce30663f5999c
Joonas Reinikka <joonas.reinikka@gmail.com>
2016-04-18 01:11:07 +0300

    Add following into user profile

--- user_profile/models.py
+++ user_profile/models.py
@@ -5,3 +5,4 @@ class UserProfile(models.Model):
   user = models.OneToOneField(User)
   description = models.TextField(max_length=3000)
   picture = models.ImageField(upload_to='media/profiles/')
+  follows = models.ManyToManyField("self", blank=True, null=True)
