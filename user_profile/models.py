a06870b6529416d14973bfe733931a86d6104d85
Joonas Reinikka <joonas.reinikka@gmail.com>
2016-04-18 00:48:41 +0300

    Create user_profile app

---
+++ user_profile/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+  user = models.OneToOneField(User)
+  description = models.TextField(max_length=3000)
+  picture = models.ImageField(upload_to='media/profiles/')
