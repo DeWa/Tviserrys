af50aa933fd51ee2409efff9410d1e7cf725129a
Joonas Reinikka <joonas.reinikka@gmail.com>
2016-05-03 14:43:07 +0300

    Remove Django-Restframework

--- tviit/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Tviit
-
-
-class TviitSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tviit
-        fields = ('pk', 'uuid', 'sender', 'content', 'created', 'modified', 'reply')
