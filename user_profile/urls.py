178373851bcb66487b89224f19e3c3dc887a8f95
Joonas Reinikka <joonas.reinikka@gmail.com>
2016-05-23 23:54:07 +0300

    Fix user_profile index page url-pattern

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^', views.ViewView.as_view(), name='profile_own_view'),
+    url(r'^$', views.ViewView.as_view(), name='profile_own_view'),
     url(r'^edit/', views.EditView.as_view(), name='profile_edit'),
     url(r'^view/', views.ViewView.as_view(), name='profile_own_view'),
     url(r'^view/(?P<user_name>\d+)/$', views.ViewView.as_view(), name='profile_view'),
