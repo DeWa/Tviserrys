ad3173b5f701cc27532103fcffe52deca67432b7
Joonas Reinikka <joonas.reinikka@gmail.com>
2016-05-23 12:30:37 +0300

    Change user_profile so picture can be null

--- user_profile/models.py
+++ user_profile/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
   user = models.OneToOneField(User)
   description = models.TextField(max_length=3000)
-  picture = models.ImageField(upload_to='media/profiles/')
+  picture = models.ImageField(blank=True, upload_to='media/profiles/')
   thumbnail = models.ImageField(
     upload_to='media/profiles/thumb/',
     max_length=500,
