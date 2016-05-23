8fdd6f8c2eb463b4d7bf9bb7372d141b97af8f1f
Joonas Reinikka <joonas.reinikka@gmail.com>
2016-05-23 15:21:20 +0300

    Add TviitForm into main View

--- tviserrys/views.py
+++ tviserrys/views.py
@@ -5,16 +5,16 @@ from django.template import RequestContext, loader
 from django.core.exceptions import PermissionDenied
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, render_to_response, render
+from tviit.models import Tviit, TviitForm
 
 
 class IndexView(View):
-    template_name = 'tviit/index.html'
+
 
     @method_decorator(login_required(login_url='/login/'))
     def get(self, request, *args, **kwargs):
-        context = RequestContext(request, {
-        })
-
-        return render(request, self.template_name, context)
-
-
+        template = loader.get_template('tviit/index.html')
+        context = {
+            'tviit_form': TviitForm,
+        }
+        return HttpResponse(template.render(context, request))
