817d1ae5cd0d4cfcba7255587d029b20f913cafd
Joonas Reinikka <joonas.reinikka@gmail.com>
2016-05-23 12:27:45 +0300

    Add thumbnail to user_profile model

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class UserProfile(models.Model):
   user = models.OneToOneField(User)
   description = models.TextField(max_length=3000)
   picture = models.ImageField(upload_to='media/profiles/')
-  follows = models.ManyToManyField("self", blank=True, null=True)
+  thumbnail = models.ImageField(
+    upload_to='media/profiles/thumb/',
+    max_length=500,
+    null=True,
+    blank=True
+  )
+  follows = models.ManyToManyField("self", blank=True)
+
+
