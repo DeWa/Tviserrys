39d0c335759781de8cf1644cdf499588441b434d
Joonas Reinikka <joonas.reinikka@gmail.com>
2016-05-23 15:21:40 +0300

    Add Tviit and profile url patterns

--- tviserrys/urls.py
+++ tviserrys/urls.py
@@ -7,6 +7,7 @@ from tviserrys.settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^tviit/', include('tviit.urls', namespace='tviit')),
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', auth_views.login),
     url(r'^logout/$', auth_views.logout),
@@ -16,6 +17,7 @@ urlpatterns = [
     url(r'^password_reset/done/$', auth_views.password_reset_done),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.password_reset_confirm),
     url(r'^reset/done/$', auth_views.password_reset_complete),
+    url(r'^profile/', include('user_profile.urls', namespace='profile')),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': MEDIA_ROOT, 'show_indexes': False}),
 ]
