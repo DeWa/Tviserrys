8ebe001a3ee3426e81372702096acf3d44827d30
Joonas Reinikka <joonas.reinikka@gmail.com>
2016-05-24 12:34:57 +0300

    Add reply url

--- tviit/urls.py
+++ tviit/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='tviit_index'),
     url(r'create/$', views.create_tviit, name="create_tviit"),
+    url(r'reply/$', views.reply_tviit, name="reply_tviit"),
 ]
