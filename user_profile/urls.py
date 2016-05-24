dab9d4e071bef5e0a771fa5a0eb7be81819bb68c
Joonas Reinikka <joonas.reinikka@gmail.com>
2016-05-24 12:33:13 +0300

    Fix user name url pattern

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^$', views.ViewView.as_view(), name='profile_own_view'),
     url(r'^edit/', views.EditView.as_view(), name='profile_edit'),
     url(r'^view/', views.ViewView.as_view(), name='profile_own_view'),
-    url(r'^view/(?P<user_name>\d+)/$', views.ViewView.as_view(), name='profile_view'),
+    url(r'^view/(?P<username>[a-zA-Z0-9_-]+)/$', views.ViewView.as_view(), name='profile_view'),
 
 ]
