50e81ead32d1c1618030a30a30e4f933178ca0ee
Joonas Reinikka <joonas.reinikka@gmail.com>
2016-05-24 16:50:09 +0300

    Add cropping and resizing functionality into profile model

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
@@ -7,6 +8,11 @@ from django.contrib.auth.models import User
 from django.forms import ModelForm
 from django import forms
 import uuid, os
+from PIL import Image
+from io import BytesIO
+from django.core.files.base import ContentFile
+from imageresize import imageresize
+from tviit.models import Tviit
 
 
 @deconstructible
@@ -32,81 +38,14 @@ class UserProfile(models.Model):
   location = models.CharField(max_length=100, blank=True)
   thumbnail = models.ImageField(
     upload_to=rename_thumbnail,
-    max_length=500,
     null=True,
     blank=True
   )
   follows = models.ManyToManyField("self", blank=True)
+  likes = models.ManyToManyField(Tviit, blank=True)
 
   def __str__(self):
     return '%s userprofile' % (self.user.username)
-
-
-  def create_thumbnail(self):
-    # original code for this method came from
-    # http://snipt.net/danfreak/generate-thumbnails-in-django-with-pil/
-
-    # If there is no image associated with this.
-    # do not create thumbnail
-    if not self.picture:
-      return
-
-    from PIL import Image
-    from cStringIO import StringIO
-    from django.core.files.uploadedfile import SimpleUploadedFile
-    import os
-
-    # Set our max thumbnail size in a tuple (max width, max height)
-    THUMBNAIL_SIZE = (99, 66)
-
-    DJANGO_TYPE = self.picture.file.content_type
-
-    if DJANGO_TYPE == 'image/jpeg':
-      PIL_TYPE = 'jpeg'
-      FILE_EXTENSION = 'jpg'
-    elif DJANGO_TYPE == 'image/png':
-      PIL_TYPE = 'png'
-      FILE_EXTENSION = 'png'
-
-    # Open original photo which we want to thumbnail using PIL's Image
-    image = Image.open(StringIO(self.picture.read()))
-
-    # We use our PIL Image object to create the thumbnail, which already
-    # has a thumbnail() convenience method that contrains proportions.
-    # Additionally, we use Image.ANTIALIAS to make the image look better.
-    # Without antialiasing the image pattern artifacts may result.
-    image.thumbnail(THUMBNAIL_SIZE, Image.ANTIALIAS)
-
-    # Save the thumbnail
-    temp_handle = StringIO()
-    image.save(temp_handle, PIL_TYPE)
-    temp_handle.seek(0)
-
-    # Save image to a SimpleUploadedFile which can be saved into
-    # ImageField
-    suf = SimpleUploadedFile(os.path.split(self.picture.name)[-1],
-                             temp_handle.read(), content_type=DJANGO_TYPE)
-    # Save SimpleUploadedFile into image field
-    self.thumbnail.save(
-      '%s_thumbnail.%s' % (os.path.splitext(suf.name)[0], FILE_EXTENSION),
-      suf,
-      save=False
-    )
-
-  def save(self, *args, **kwargs):
-    force_update = False
-
-    # If the instance already has been saved, it has an id and we set
-    # force_update to True
-    if self.id:
-      orig = UserProfile.objects.get(pk=self.pk)
-      if orig.picture != self.picture:
-        self.create_thumbnail()
-      force_update = True
-
-    # Force an UPDATE SQL query if we're editing the image to avoid integrity exception
-    super(UserProfile, self).save(force_update=force_update)
-
 
 
 # Create UserProfile when user is created
@@ -116,6 +55,32 @@ def create_profile(sender, **kwargs):
     if kwargs["created"]:
       profile = UserProfile(user=user)
       profile.save()
+
+# Resize image and crop it
+@receiver(post_save, sender=UserProfile)
+def resize_and_crop(sender, **kwargs):
+    profile = kwargs["instance"]
+    picture = profile.picture
+    image = Image.open(picture.path)
+
+    # Get the right ratio
+    width, height = image.size
+    ratio = width / height
+
+    if width <= height:
+        new_width = 200
+        new_height = new_width / ratio
+    else:
+        new_height = 200
+        new_width = new_height * ratio
+
+
+    new_height = int(round(new_height))
+    new_width = int(round(new_width))
+
+    image = image.resize((new_width, new_height), Image.ANTIALIAS)
+    image = image.crop((0, 0, 200, 200))
+    image.save(picture.path)
 
 
 class EditProfileForm(ModelForm):
