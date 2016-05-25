35c1a0ff7109926187f796d93bfdec12b1342720
Joonas Reinikka <joonas.reinikka@gmail.com>
2016-05-25 19:51:05 +0300

    Fix replying to tviits

--- tviit/views.py
+++ tviit/views.py
@@ -6,6 +6,7 @@ from django.core.exceptions import PermissionDenied
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, render_to_response, render
 from .models import Tviit, TviitForm
+from user_profile.models import UserProfile
 import uuid
 
 
@@ -14,8 +15,9 @@ class IndexView(View):
     @method_decorator(login_required(login_url='/login/'))
     def get(self, request, *args, **kwargs):
         template = loader.get_template('tviit/index.html')
+        profile = UserProfile.objects.get(user=request.user)
         context = {
-
+            'profile': profile,
         }
         return HttpResponse(template.render(context, request))
 
@@ -29,7 +31,11 @@ def create_tviit(request):
             f = form.save(commit=False)
             f.uuid = uuid.uuid4().int
             f.sender = request.user
-            f.image = request.FILES['image']
+
+            # If there's image
+            if 'image' in request.FILES:
+                f.image = request.FILES['image']
+
             f.save()
             return HttpResponse(status=201) # Created
 
@@ -37,51 +43,33 @@ def create_tviit(request):
     else:
         return HttpResponse(status=400) # Bad Content
 
-'''
+
 def reply_tviit(request):
     if request.method == 'POST':
-        post_text = request.POST.get('the_post')
-        response_data = {}
+        form = TviitForm(request.POST, request.FILES)
 
-        post = Post(text=post_text, author=request.user)
-        post.save()
+        if form.is_valid():
+            if request.POST.get('reply'):
+                reply = Tviit.objects.get(uuid=request.POST.get('reply'))
+                if not reply:
+                    return HttpResponse(status=404)  # Not found
+            else:
+                return HttpResponse(status=400)  # Bad Content
 
-        response_data['result'] = 'Create post successful!'
-        response_data['postpk'] = post.pk
-        response_data['text'] = post.text
-        response_data['created'] = post.created.strftime('%B %d, %Y %I:%M %p')
-        response_data['author'] = post.author.username
 
-        return HttpResponse(
-            json.dumps(response_data),
-            content_type="application/json"
-        )
+            f = form.save(commit=False)
+            f.uuid = uuid.uuid4().int
+            f.sender = request.user
+            f.reply = reply
+
+
+            # If there's image
+            if 'image' in request.FILES:
+                f.image = request.FILES['image']
+
+            f.save()
+            return HttpResponse(status=201) # Created
+
+
     else:
-        return HttpResponse(
-            json.dumps({"nothing to see": "this isn't happening"}),
-            content_type="application/json"
-        )
-def like_tviit(request):
-    if request.method == 'POST':
-        post_text = request.POST.get('the_post')
-        response_data = {}
-
-        post = Post(text=post_text, author=request.user)
-        post.save()
-
-        response_data['result'] = 'Create post successful!'
-        response_data['postpk'] = post.pk
-        response_data['text'] = post.text
-        response_data['created'] = post.created.strftime('%B %d, %Y %I:%M %p')
-        response_data['author'] = post.author.username
-
-        return HttpResponse(
-            json.dumps(response_data),
-            content_type="application/json"
-        )
-    else:
-        return HttpResponse(
-            json.dumps({"nothing to see": "this isn't happening"}),
-            content_type="application/json"
-        )
-'''
+        return HttpResponse(status=400) # Bad Content
